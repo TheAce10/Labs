bcd2d0c34b6e09df9fe0939800664962fade117f
Elikem <99412803+TheAce10@users.noreply.github.com>
2024-12-19 12:28:01 +0000

    Initial base conversion to Cpp

---
+++ Cache Simulator Project/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void)
+{
+    long address;
+    int loadstore, icount;
+    char marker;
+    std::cin>>marker>>address;
+    std::cout<<marker<<address;
+    return (0);
+}
